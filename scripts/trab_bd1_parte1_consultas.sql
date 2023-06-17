c432e7854ce26cf0d856234b94a7c40cec829ee5
LuizWillner <luizwillner@id.uff.br>
2023-06-17 13:26:00 -0300

    Questões de consultas adicionadas como comentários

---
+++ scripts/trab_bd1_parte1_consultas.sql
@@ -0,0 +1,41 @@
+-- Trabalho Banco de Dados 1 - Consultas --
+
+
+-- Q1: Listar o nome e a cidade das escolas onde todos os alunos
+--     residam na mesma cidade onde a escola está localizada.
+
+
+-- Q2: Listar o nome e matrícula dos alunos sem nenhum contato cadastrado.
+
+
+-- Q3: Listar o código e nome das turmas com mais de 5 alunos.
+
+
+-- Q4: Listar o código, nome e titulação dos professores que ministram
+--     aulas para pelo menos três turmas diferentes.
+
+
+-- Q5: Listar por disciplina o número de professores que podem ministrá-la
+--     e quantos efetivamente ministram a mesma para uma turma.
+
+
+-- Q6: Listar o nome da escola e o nome dos diretores de escola que residem
+--     em cidades diferentes da cidade da escola.
+
+
+-- Q7: Listar por escola o número de turmas e o número de professores que
+--     ministram alguma disciplina para turmas da escola em questão.
+
+
+
+-- Q8: Listar por escola a razão entre o número de alunos da escola e o
+--     número de professores que ministram alguma disciplina na escola em questão.
+
+
+
+-- Q9: Listar todos os contatos dos alunos informando a matrícula e nome do aluno,
+--     nome e telefone do contato, ordenado por matrícula do aluno e nome do contato.
+
+
+
+-- Q10: Listar todos os professores que ministram disciplinas para apenas uma turma
