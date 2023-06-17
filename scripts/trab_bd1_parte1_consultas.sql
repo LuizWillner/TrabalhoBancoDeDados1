60b26714f2684505a13f051a74af7e115797bf40
LuizWillner <luizwillner@id.uff.br>
2023-06-17 18:09:19 -0300

    Q3 e Q4 das consultas feitas

--- scripts/trab_bd1_parte1_consultas.sql
+++ scripts/trab_bd1_parte1_consultas.sql
@@ -6,7 +6,8 @@
 
 
 -- Q2: Listar o nome e matrícula dos alunos sem nenhum contato cadastrado.
-select pessoa.nome, aluno.matricula
+
+select pessoa.codPessoa, pessoa.nome, aluno.matricula
 from pessoa
 join aluno on pessoa.codPessoa = aluno.codPessoa 
 where aluno.codPessoa not in (
@@ -14,11 +15,31 @@ where aluno.codPessoa not in (
 	from contato
 );
 
+
 -- Q3: Listar o código e nome das turmas com mais de 5 alunos.
+
+select alunos_por_turma.codTurma, turma.nome 
+from (
+	select aluno.codTurma, count(*) as quant_alunos
+	from aluno
+	group by aluno.codTurma
+) as alunos_por_turma
+join turma on turma.codTurma = alunos_por_turma.codTurma
+where alunos_por_turma.quant_alunos > 5;
 
 
 -- Q4: Listar o código, nome e titulação dos professores que ministram
 --     aulas para pelo menos três turmas diferentes.
+
+select turma_por_prof.codProf, pessoa.nome, professor.titulacao 
+from (
+	select ministrar_turma.codProf, count(*) as quant_turmas
+	from ministrar_turma
+	group by ministrar_turma.codProf
+) as turma_por_prof
+join pessoa on turma_por_prof.codProf = pessoa.codPessoa
+join professor on pessoa.codPessoa = professor.codPessoa 
+where turma_por_prof.quant_turmas >= 3;
 
 
 -- Q5: Listar por disciplina o número de professores que podem ministrá-la
@@ -33,15 +54,12 @@ where aluno.codPessoa not in (
 --     ministram alguma disciplina para turmas da escola em questão.
 
 
-
 -- Q8: Listar por escola a razão entre o número de alunos da escola e o
 --     número de professores que ministram alguma disciplina na escola em questão.
 
 
-
 -- Q9: Listar todos os contatos dos alunos informando a matrícula e nome do aluno,
 --     nome e telefone do contato, ordenado por matrícula do aluno e nome do contato.
-
 
 
 -- Q10: Listar todos os professores que ministram disciplinas para apenas uma turma
