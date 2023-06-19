387758151aa22c40e26e8a0ec0d2d8b07cb7b201
LuizWillner <luizwillner@id.uff.br>
2023-06-19 15:40:41 -0300

    Questões de atualização adicionadas como comentários no script

---
+++ scripts/trab_bd1_parte2_alteracoes.sql
@@ -0,0 +1,14 @@
+-- Q1: Alterar todos os contatos vinculados a um aluno A para um aluno B.
+
+
+-- Q2:  Forneça um conjunto de instruções SQL para remover da base de 
+--      dados um professor qualquer. Caso o professor seja diretor de 
+--      uma escola um novo diretor deverá ser definido.
+
+
+-- Q3:  Considerando que um Professor P1 precisou sair de licença médica,
+--      substituí-lo por outro Professor P2 em todas as turmas onde P1 ministra
+--      disciplinas. Importante: Caso o Professor P2 não esteja vinculado a todas
+--      as disciplinas ministradas pelo Professor P1, inclua a informação de que  
+--      o Professor P2 é capaz de ministrar todas as disciplinas ministradas pelo
+--      Professor P1.
